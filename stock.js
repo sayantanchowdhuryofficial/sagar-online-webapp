44b44e190cea461ac7b17e0ff9266ee3f3de25d1
sayantanchowdhuryofficial <sayantan.chowdhury.official@gmail.com>
2026-03-11 00:58:01 +0530

    Update and rename dashboard.js to stock.js

--- stock.js
+++ stock.js
@@ -5,11 +5,11 @@ function addProduct(){
 let name=document.getElementById("pname").value
 let qty=parseInt(document.getElementById("pqty").value)
 let cat=document.getElementById("pcat").value
+let low=parseInt(document.getElementById("plow").value)
 
-products.push({name,qty,cat})
+products.push({name,qty,cat,low})
 
 saveProducts()
-
 renderProducts()
 
 }
@@ -18,23 +18,33 @@ function renderProducts(){
 
 let table=document.getElementById("productTable")
 
-table.innerHTML="<tr><th>Name</th><th>Qty</th><th>Category</th></tr>"
+table.innerHTML="<tr><th>Name</th><th>Qty</th><th>Category</th><th>Low Alert</th></tr>"
+
+let lowCount=0
 
 products.forEach(p=>{
 
+let alert=p.qty<=p.low?"style='background:red'":""
+
+if(p.qty<=p.low) lowCount++
+
 table.innerHTML+=`
 
-<tr>
+<tr ${alert}>
 
 <td>${p.name}</td>
 <td>${p.qty}</td>
 <td>${p.cat}</td>
+<td>${p.low}</td>
 
 </tr>
 
 `
 
 })
+
+document.getElementById("totalProducts").innerText=products.length
+document.getElementById("lowStock").innerText=lowCount
 
 }
 
